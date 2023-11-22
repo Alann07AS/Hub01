186479bc2561b64464a45e170c52df533c467939
Alann <AlannSchnebelen@hotmail.com>
2023-11-22 11:03:59 +0100

    Automatic All Save of Hub01/ at mer. 22 nov. 2023 11:03:59 CET

--- static/js/init.js
+++ static/js/init.js
@@ -1,1 +1,5 @@
+import { trad } from "./translate.js";
+
 console.log("init HUB01 par https://github.com/Alann07AS/Hub01");
+
+trad()
--- static/js/translate.js
+++ static/js/translate.js
@@ -1,13 +1,16 @@
-var userLang = (navigator.languages || navigator.userLanguages)[0].split("-")[0];
+import { translates } from "../translation/fr.tl";
 
-var translations = document.getElementById('translations_login_content').innerHTML;
-console.log(translations);
-var translationsObj = JSON.parse(translations);
+export function trad() {
+    var userLang = (navigator.languages || navigator.userLanguages)[0].split("-")[0];
+    userLang = "fr" 
+    var translations = translates;
 
-var to_translates = document.getElementsByClassName("translate");
+    // var to_translates = document.getElementsByClassName("translate");
+    var to_translates = document.querySelectorAll('[tl]');
+    console.log(to_translates);
 
-for (const to_t of to_translates) {
-    
-    to_t.textContent = translationsObj[to_t.id][userLang] || to_t.textContent;
+    for (const to_t of to_translates) {
+        to_t.textContent = translations[to_t.tl][userLang] || to_t.textContent;
+    }
 
 }
